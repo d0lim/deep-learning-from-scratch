4fe33de9c7a6dcfd32ca80ffbefe5520ab3d6668
DoLim <limdongyoung0@gmail.com>
2021-01-12 16:52:23 +0900

    study: Implemented neural network step by step

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pylab as plt
 
 
 def AND(x1, x2):
@@ -41,8 +42,52 @@ def XOR(x1, x2):
     return y
 
 
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def identity_function(x):
+    return x
+
+
 if __name__ == "__main__":
-    print(XOR(0, 0))
-    print(XOR(0, 1))
-    print(XOR(1, 0))
-    print(XOR(1, 1))
+    X = np.array([1.0, 0.5])
+    W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    B1 = np.array([0.1, 0.2, 0.3])
+
+    print(W1.shape)
+    print(X.shape)
+    print(B1.shape)
+
+    A1 = np.dot(X, W1) + B1
+
+    Z1 = sigmoid(A1)
+
+    print(A1)
+    print(Z1)
+
+    W2 = np.array([[0.1, 0.4], [0.2, 0.4], [0.3, 0.6]])
+    B2 = np.array([0.1, 0.2])
+
+    print(Z1.shape)
+    print(W2.shape)
+    print(B2.shape)
+
+    A2 = np.dot(Z1, W2) + B2
+    Z2 = sigmoid(A2)
+
+    W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+    B3 = np.array([0.1, 0.2])
+
+    A3 = np.dot(Z2, W3) + B3
+    Y = identity_function(A3) # or Y = A3
+
+    print(Y)
