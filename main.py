af06e9b50f3a6fa8893f4d8758182cea5e231daa
DoLim <limdongyoung0@gmail.com>
2021-01-13 15:12:35 +0900

    study: defined sample weight network

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pickle
 import sys, os
 import numpy as np
 import matplotlib.pylab as plt
@@ -69,19 +70,22 @@ def softmax(a):
     return y
 
 
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(
+        normalize=True, flatten=True, one_hot_label=False
+    )
+
+    return x_test, t_test
+
+
 def init_network():
-    network = {}
-    network["W1"] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-    network["b1"] = np.array([0.1, 0.2, 0.3])
-    network["W2"] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-    network["b2"] = np.array([0.1, 0.2])
-    network["W3"] = np.array([[0.1, 0.3], [0.2, 0.4]])
-    network["b3"] = np.array([0.1, 0.2])
+    with open("./sample_weight.pkl", "rb") as f:
+        network = pickle.load(f)
 
     return network
 
 
-def forward(network, x):
+def predict(network, x):
     W1, W2, W3 = network["W1"], network["W2"], network["W3"]
     b1, b2, b3 = network["b1"], network["b2"], network["b3"]
 
@@ -90,15 +94,20 @@ def forward(network, x):
     a2 = np.dot(z1, W2) + b2
     z2 = sigmoid(a2)
     a3 = np.dot(z2, W3) + b3
-    y = identity_function(a3)
+    y = softmax(a3)
 
     return y
 
 
 if __name__ == "__main__":
-    (x_train, t_train) , (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+    x, t = get_data()
+    network = init_network()
 
-    print(x_train.shape)
-    print(t_train.shape)
-    print(x_test.shape)
-    print(t_test.shape)
+    accuracy_cnt = 0
+    for i in range(len(x)):
+        y = predict(network, x[i])
+        p = np.argmax(y)
+        if p == t[i]:
+            accuracy_cnt += 1
+
+    print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
