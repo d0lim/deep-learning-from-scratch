cb2d4583ce981aea2f29ff5a6639b05d7f626957
DoLim <limdongyoung0@gmail.com>
2021-01-14 16:30:31 +0900

    study: implemented sum squares error function

--- main.py
+++ main.py
@@ -70,6 +70,10 @@ def softmax(a):
     return y
 
 
+def sum_squares_error(y, t):
+    return 0.5 * np.sum((y - t) ** 2)
+
+
 def get_data():
     (x_train, t_train), (x_test, t_test) = load_mnist(
         normalize=True, flatten=True, one_hot_label=False
