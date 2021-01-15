00a64f6b7065e45d593345e0404aeedf0139ee8a
DoLim <limdongyoung0@gmail.com>
2021-01-15 16:40:53 +0900

    study: applied cross entropy error function

--- main.py
+++ main.py
@@ -74,6 +74,11 @@ def sum_squares_error(y, t):
     return 0.5 * np.sum((y - t) ** 2)
 
 
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return np.sum(t * np.log(y + delta))
+
+
 def get_data():
     (x_train, t_train), (x_test, t_test) = load_mnist(
         normalize=True, flatten=True, one_hot_label=False
