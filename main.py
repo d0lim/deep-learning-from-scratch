20d5769cfc42c0a1253956d0ff70c557d4b79070
DoLim <limdongyoung0@gmail.com>
2021-01-18 16:09:25 +0900

    study: organized directory and implementing simple net

--- main.py
+++ main.py
@@ -1,163 +1,43 @@
-import pickle
+# coding: utf-8
+
 import sys, os
 import numpy as np
-import matplotlib.pylab as plt
-from dataset.mnist import load_mnist
+from common.functions import softmax, cross_entropy_error
+from common.gradient import numerical_gradient
 
 
-def AND(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([0.5, 0.5])
-    b = -0.7
-    tmp = np.sum(w * x) + b
-    if tmp <= 0:
-        return 0
-    else:
-        return 1
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3)  # 정규분포로 초기화
 
+    def predict(self, x):
+        return np.dot(x, self.W)
 
-def NAND(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([-0.5, -0.5])
-    b = 0.7
-    tmp = np.sum(w * x) + b
-    if tmp <= 0:
-        return 0
-    else:
-        return 1
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
 
-
-def OR(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([0.5, 0.5])
-    b = -0.2
-    tmp = np.sum(w * x) + b
-    if tmp <= 0:
-        return 0
-    else:
-        return 1
-
-
-def XOR(x1, x2):
-    s1 = NAND(x1, x2)
-    s2 = OR(x1, x2)
-    y = AND(s1, s2)
-    return y
-
-
-def step_function(x):
-    return np.array(x > 0, dtype=np.int)
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
-def relu(x):
-    return np.maximum(0, x)
-
-
-def identity_function(x):
-    return x
-
-
-def softmax(a):
-    c = np.max(a)
-    exp_a = np.exp(a - c)
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
-
-    return y
-
-
-def sum_squares_error(y, t):
-    return 0.5 * np.sum((y - t) ** 2)
-
-
-def cross_entropy_error(y, t):
-    if y.ndim == 1:
-        t = t.reshape(1, t.size)
-        y = y.reshape(1, y.size)
-
-    batch_size = y.shape[0]
-
-    return -np.sum(t * np.log(y + 1e-7)) / batch_size
-
-
-def numerical_diff(f, x):
-    h = 1e-4  # 0.0001
-    return (f(x + h) - f(x - h)) / (2 * h)
-
-
-def numerical_gradient(f, x):
-    h = 1e-4  # 0.0001
-    grad = np.zeros_like(x)
-
-    for idx in range(x.size):
-        tmp_val = x[idx]
-        # f(x+h) 계산
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
-
-        # f(x-h) 계산
-        x[idx] = tmp_val - h
-        fxh2 = f(x)
-
-        grad[idx] = (fxh1 - fxh2) / (2 * h)
-        x[idx] = tmp_val  # 값 복원
-
-    return grad
-
-
-def gradient_descent(f, init_x, lr=0.01, step_num=100):
-    x = init_x
-
-    for i in range(step_num):
-        grad = numerical_gradient(f, x)
-        x -= lr * grad
-
-    return x
-
-
-def get_data():
-    (x_train, t_train), (x_test, t_test) = load_mnist(
-        normalize=True, flatten=True, one_hot_label=False
-    )
-
-    return x_test, t_test
-
-
-def init_network():
-    with open("./sample_weight.pkl", "rb") as f:
-        network = pickle.load(f)
-
-    return network
-
-
-def predict(network, x):
-    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
-    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
-
-    a1 = np.dot(x, W1) + b1
-    z1 = sigmoid(a1)
-    a2 = np.dot(z1, W2) + b2
-    z2 = sigmoid(a2)
-    a3 = np.dot(z2, W3) + b3
-    y = softmax(a3)
-
-    return y
+        return loss
 
 
 if __name__ == "__main__":
-    x, t = get_data()
-    network = init_network()
+    net = simpleNet()
+    print(net.W)
 
-    batch_size = 100  # batch size
-    accuracy_cnt = 0
-    for i in range(0, len(x), batch_size):
-        x_batch = x[i : i + batch_size]
-        y = predict(network, x_batch)
-        p = np.argmax(y, axis=1)
-        accuracy_cnt += np.sum(p == t[i : i + batch_size])
+    x = np.array([0.6, 0.9])
+    p = net.predict(x)
+    print(p)
 
-    print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+    print(np.argmax(p))
+
+    t = np.array([0, 0, 1])
+
+    print(net.loss(x, t))
+
+    def f(W):
+        return net.loss(x, t)
+
+    dW = numerical_gradient(f, net.W)
+
+    print(dW)
