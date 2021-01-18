2eb12add8bd475d9fc30283aab9362ea64aba6d4
DoLim <limdongyoung0@gmail.com>
2021-01-18 14:45:38 +0900

    study: defined various calculous functions

--- main.py
+++ main.py
@@ -84,6 +84,41 @@ def cross_entropy_error(y, t):
     return -np.sum(t * np.log(y + 1e-7)) / batch_size
 
 
+def numerical_diff(f, x):
+    h = 1e-4  # 0.0001
+    return (f(x + h) - f(x - h)) / (2 * h)
+
+
+def numerical_gradient(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h) 계산
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val  # 값 복원
+
+    return grad
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x
+
+
 def get_data():
     (x_train, t_train), (x_test, t_test) = load_mnist(
         normalize=True, flatten=True, one_hot_label=False
