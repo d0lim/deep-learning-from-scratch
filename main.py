c58922fe58a32b6a5cdaa7e3a013aab408a506f6
DoLim <limdongyoung0@gmail.com>
2021-01-22 16:47:46 +0900

    study: implemented two layer net without backward

--- main.py
+++ main.py
@@ -2,41 +2,144 @@
 
 import sys, os
 import numpy as np
-from common.functions import softmax, cross_entropy_error
+from common.functions import sigmoid, softmax, cross_entropy_error
 from common.gradient import numerical_gradient
 
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
 
-class simpleNet:
-    def __init__(self):
-        self.W = np.random.randn(2, 3)  # 정규분포로 초기화
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # 가중치 초기화
+        self.params = {}
+        self.params["W1"] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params["b1"] = np.zeros(hidden_size)
+        self.params["W2"] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params["b2"] = np.zeros(output_size)
 
     def predict(self, x):
-        return np.dot(x, self.W)
+        W1, W2 = self.params["W1"], self.params["W2"]
+        b1, b2 = self.params["b1"], self.params["b2"]
 
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+
+    # x : 입력 데이터, t : 정답 레이블
     def loss(self, x, t):
-        z = self.predict(x)
-        y = softmax(z)
-        loss = cross_entropy_error(y, t)
+        y = self.predict(x)
 
-        return loss
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    # x : 입력 데이터, t : 정답 레이블
+    def numerical_gradient(self, x, t):
+
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        print("Start numerical gradient 1")
+        grads["W1"] = numerical_gradient(loss_W, self.params["W1"])
+        print("Start numerical gradient 2")
+        grads["b1"] = numerical_gradient(loss_W, self.params["b1"])
+        print("Start numerical gradient 3")
+        grads["W2"] = numerical_gradient(loss_W, self.params["W2"])
+        print("Start numerical gradient 4")
+        grads["b2"] = numerical_gradient(loss_W, self.params["b2"])
+
+        return grads
+
+    # def gradient(self, x, t):
+    #     W1, W2 = self.params['W1'], self.params['W2']
+    #     b1, b2 = self.params['b1'], self.params['b2']
+    #     grads = {}
+
+    #     batch_num = x.shape[0]
+
+    #     # forward
+    #     a1 = np.dot(x, W1) + b1
+    #     z1 = sigmoid(a1)
+    #     a2 = np.dot(z1, W2) + b2
+    #     y = softmax(a2)
+
+    #     # backward
+    #     dy = (y - t) / batch_num
+    #     grads['W2'] = np.dot(z1.T, dy)
+    #     grads['b2'] = np.sum(dy, axis=0)
+
+    #     da1 = np.dot(dy, W2.T)
+    #     dz1 = sigmoid_grad(a1) * da1
+    #     grads['W1'] = np.dot(x.T, dz1)
+    #     grads['b1'] = np.sum(dz1, axis=0)
+
+    #     return grads
 
 
 if __name__ == "__main__":
-    net = simpleNet()
-    print(net.W)
+    # 데이터 읽기
+    (x_train, t_train), (x_test, t_test) = load_mnist(
+        normalize=True, one_hot_label=True
+    )
 
-    x = np.array([0.6, 0.9])
-    p = net.predict(x)
-    print(p)
+    network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-    print(np.argmax(p))
+    # 하이퍼파라미터
+    iters_num = 10000  # 반복 횟수를 적절히 설정한다.
+    train_size = x_train.shape[0]
+    batch_size = 100  # 미니배치 크기
+    learning_rate = 0.1
 
-    t = np.array([0, 0, 1])
+    train_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
 
-    print(net.loss(x, t))
+    # 1에폭당 반복 수
+    iter_per_epoch = max(train_size / batch_size, 1)
 
-    f = lambda w: net.loss(x, t)
+    for i in range(iters_num):
+        # 미니배치 획득
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
 
-    dW = numerical_gradient(f, net.W)
+        # 기울기 계산
+        grad = network.numerical_gradient(x_batch, t_batch)
+        # grad = network.gradient(x_batch, t_batch)
 
-    print(dW)
+        # 매개변수 갱신
+        for key in ("W1", "b1", "W2", "b2"):
+            network.params[key] -= learning_rate * grad[key]
+
+        # 학습 경과 기록
+        loss = network.loss(x_batch, t_batch)
+        train_loss_list.append(loss)
+
+        # 1에폭당 정확도 계산
+        if i % iter_per_epoch == 0:
+            train_acc = network.accuracy(x_train, t_train)
+            test_acc = network.accuracy(x_test, t_test)
+            train_acc_list.append(train_acc)
+            test_acc_list.append(test_acc)
+            print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+    # 그래프 그리기
+    markers = {"train": "o", "test": "s"}
+    x = np.arange(len(train_acc_list))
+    plt.plot(x, train_acc_list, label="train acc")
+    plt.plot(x, test_acc_list, label="test acc", linestyle="--")
+    plt.xlabel("epochs")
+    plt.ylabel("accuracy")
+    plt.ylim(0, 1.0)
+    plt.legend(loc="lower right")
+    plt.show()
