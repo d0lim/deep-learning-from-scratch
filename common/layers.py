684e9c42adbb054326938fbdb0897d0e9dd2f07f
DoLim <limdongyoung0@gmail.com>
2021-08-10 17:04:49 +0900

    feat: back propagation implementation

--- common/layers.py
+++ common/layers.py
@@ -1,3 +1,4 @@
+from common.functions import cross_entropy_error, softmax
 import numpy as np
 
 
@@ -53,5 +54,24 @@ class Affine:
         dx = np.dot(dout, self.W.T)
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
+
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None  # 손실
+        self.y = None  # softmax의 출력
+        self.t = None  # 정답 레이블(원-핫 벡터)
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
 
         return dx
