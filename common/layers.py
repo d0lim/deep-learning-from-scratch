9217481d0c9febf5f8d180ec80a9ff1b220412d0
DoLim <limdongyoung0@gmail.com>
2021-08-16 14:30:17 +0900

    feat: implement forward for CNN

--- common/layers.py
+++ common/layers.py
@@ -1,3 +1,4 @@
+from common.util import im2col
 from common.functions import cross_entropy_error, softmax
 import numpy as np
 
@@ -198,3 +199,54 @@ class BatchNormalization:
         self.dbeta = dbeta
 
         return dx
+
+
+20
+
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = int(1 + (H + 2 * self.pad - FH) / self.stride)
+        out_w = int(1 + (H + 2 * self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T  # 필터 전개
+        out = np.dot(col, col_W) + self.b
+
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)  # reshape -1 주면 자동
+
+        return out
+
+
+
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1+ (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        # 전개 (1)
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+
+        # 최댓값 (2)
+        out = np.max(col, axis=1)
+
+        # 성형 (3)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        return out
